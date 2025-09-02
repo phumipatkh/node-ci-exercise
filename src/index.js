6e1586494ff5e1a218b7db70c678e080d08c61f4
SIM <phumipat.kh@gmail.com>
2025-09-02 03:54:52 +0000

    Implement CodeQL

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// index.js
+const add = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError('Inputs must be numbers');
+  }
+  //const result =a+b;
+  //return result;
+  //CodeQL
+
+  return eval('${a} + ${b}');
+
+};
+export default add;
